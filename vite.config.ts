2f8b3cc2067797a12c2b8f84f83fbabd82636f2a
led-dotcom <74643143+led-dotcom@users.noreply.github.com>
2024-04-28 13:25:51 -0400

    Add vitest types to tsconfig.json and setup test setup file

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 import * as path from 'path'
@@ -14,5 +14,12 @@ export default defineConfig({
       }
     ],
     extensions: ['.js', 'jsx', '.json', '.ts', '.tsx']
+  },
+  test: {
+    // 👋 add the line below to add jsdom to vite
+    environment: 'jsdom',
+    globals: true,
+    include: ['src/**/*.test.{ts,tsx}'],
+    setupFiles: './src/tests/setup.ts'
   }
 })
